091b8a9c8fc31cc4983a3a9304a1b99d51d3cd43
AL-sadperson <146556614+AL-sadperson@users.noreply.github.com>
2026-05-29 08:01:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-gghh
+const btn = document.getElementById('chaos-btn');
+
+btn.addEventListener('click', () => {
+    document.body.style.filter = 'invert(1)';
+    setTimeout(() => {
+        document.body.style.filter = 'none';
+        alert("بيل يرى كل شيء الآن...");
+    }, 500);
+});
